288bd4d805ca6384df14e294574c85981785f982
MethuParoi <paroimethu@gmail.com>
2025-01-14 00:06:49 +0600

    tailwind css added to the project

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["poppins", "serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#0F4C75",
+          secondary: "#3282B8",
+          // accent: "#BBE1FA",
+          accent: "#FFC7C7",
+          neutral: "#F1F8E8",
+          "base-100": "#ffffff",
+          "base-200": "#EEEEEE",
+        },
+      },
+      {
+        dark: {
+          primary: "#1E293B",
+          secondary: "#334155",
+          accent: "#64748B",
+          neutral: "#1E293B",
+          "base-100": "#0F172A",
+          "base-200": "#1E293B",
+        },
+      },
+    ],
+  },
+};
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };

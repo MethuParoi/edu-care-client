0d1db57b1d61855de0859c1df495c6e58b17888c
MethuParoi <paroimethu@gmail.com>
2025-01-17 12:04:45 +0600

    user search feature implemented

--- src/components/admin-dashboard/manage-user/UserRow.jsx
+++ src/components/admin-dashboard/manage-user/UserRow.jsx
@@ -1,8 +1,26 @@
-import { useContext } from "react";
-import { AuthContext } from "../../../provider/AuthProvider";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { toast } from "react-toastify";
 
-const UserRow = ({ username, email, subscription_status, _id }) => {
-  const { link } = useContext(AuthContext);
+const UserRow = ({
+  username,
+  email,
+  subscription_status,
+  userid,
+  user_role,
+  setLoading,
+  refetch,
+}) => {
+  const axiosSecure = useAxiosSecure();
+
+  const handleMakeAdmin = (id) => {
+    axiosSecure.patch(`/user/make-admin/${id}`).then((res) => {
+      setLoading(false);
+      if (res.status === 200) {
+        toast.success("Admin made successfully!");
+        refetch();
+      }
+    });
+  };
 
   return (
     <div className="grid grid-cols-4 gap-y-4 justify-items-center w-[1000px] md:w-[90%] xl:w-[1000px] mx-auto bg-[#3282B8] p-4 my-4 rounded-xl">
@@ -17,8 +35,15 @@ const UserRow = ({ username, email, subscription_status, _id }) => {
       </p>
 
       <div className="flex space-x-4">
-        <button className="hover:bg-red-500 bg-red-400 text-gray-100 px-4 py-1 rounded-lg">
-          Make Admin
+        <button
+          disabled={user_role === "admin"}
+          onClick={() => {
+            handleMakeAdmin(userid);
+            setLoading(true);
+          }}
+          className="hover:bg-red-500 bg-red-400 text-gray-100 px-4 py-1 rounded-lg"
+        >
+          {user_role === "admin" ? "Admin" : "Make Admin"}
         </button>
       </div>
     </div>
